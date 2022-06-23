7afd37ca1ee807b8094d4e36e9cb03293fbb0de5
Magicoo51889 <jackot11f@gmail.com>
2022-06-23 09:05:18 +0100

    Update screenWrap.ts

--- src/components/screenWrap.ts
+++ src/components/screenWrap.ts
@@ -1,4 +1,4 @@
-export default function screenWrap (sprite, scene) {
+export default function screenWrap (sprite, scene) { // when it goes to one edge it gets stuck as it is called every frame, so is always set at an edge. 
     if (sprite.x <= 0){
         sprite.x = scene.width;
         console.log("wrapping left")
